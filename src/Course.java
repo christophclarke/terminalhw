6b200830f4eb433d89cd147541e3b07fbe283955
christopherclarke <chris.clarke97@gmail.com>
2018-02-08 16:34:13 -0600

    initial Course variables added

--- src/Course.java
+++ src/Course.java
@@ -1,4 +1,11 @@
-public class Course {
+import java.util.ArrayList;
+
+class Course {
+
+    private String department;
+    private int courseNumber;
+    ArrayList<Test> testList = new ArrayList<>();
+    ArrayList<Assignment> assignmentList = new ArrayList<>();
 
 
 }
