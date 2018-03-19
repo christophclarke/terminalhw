47b6440fb9d8549969f9c75c2ec318f90128afa6
christophclarke <chris.clarke97@gmail.com>
2018-03-19 14:46:33 -0500

    created student object to package semesters into a parent object

---
+++ src/objects/Student.java
@@ -0,0 +1,54 @@
+package objects;
+
+import java.io.Console;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Student implements Serializable {
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<Semester> semesterList = new ArrayList<>();
+
+    public Student(Console console) {
+
+        console.format("--- Creating Student ---%n");
+        this.firstName = console.readLine("First Name > %n");
+        this.lastName = console.readLine("Last Name > %n");
+        console.format("--- Student Initialized ---%n");
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Semester> getSemesterList() {
+        return semesterList;
+    }
+
+    public void setSemesterList(ArrayList<Semester> semesterList) {
+        this.semesterList = semesterList;
+    }
+
+    public String toFileName() {
+
+        return firstName + "_" + lastName + ".student";
+
+    }
+
+
+
+}
