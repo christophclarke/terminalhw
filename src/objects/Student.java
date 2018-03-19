394e3cda964bd4388a28eb197e88051dbd5804ad
christophclarke <chris.clarke97@gmail.com>
2018-03-19 16:11:58 -0500

    UX deisgn fixes

--- src/objects/Student.java
+++ src/objects/Student.java
@@ -12,10 +12,10 @@ public class Student implements Serializable {
 
     public Student(Console console) {
 
-        console.format("%n--- Creating Student ---");
-        this.firstName = console.readLine("%n%nFirst Name > ");
-        this.lastName = console.readLine("%nLast Name > ");
-        console.format("%n%n--- Student Initialized ---%n%n");
+        console.format("%n--- Creating Student ---%n");
+        this.firstName = console.readLine("First Name > ");
+        this.lastName = console.readLine("Last Name > ");
+        console.format("--- Student Initialized ---%n%n");
 
     }
 
